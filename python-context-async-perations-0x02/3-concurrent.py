86447aeca4acf6e41fd5d319275a817a2133e9e2
Emmanuel5012 <osomisakin@gmail.com>
2025-07-12 06:49:30 +0100

    Concurrent Asynchronous Database Queries and coverting mysql db to sqlite db

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -5,23 +5,27 @@ async def async_fetch_users():
     async with aiosqlite.connect("users.db") as db:
         async with db.execute("SELECT * FROM users") as cursor:
             rows = await cursor.fetchall()
-            print("\n📦 All Users:")
-            for row in rows:
-                print(row)
+    return rows  # ✅ Added return
 
 async def async_fetch_older_users():
     async with aiosqlite.connect("users.db") as db:
         async with db.execute("SELECT * FROM users WHERE age > 40") as cursor:
             rows = await cursor.fetchall()
-            print("\n🎯 Users Older Than 40:")
-            for row in rows:
-                print(row)
+    return rows  # ✅ Added return
 
 async def fetch_concurrently():
-    await asyncio.gather(
+    users, older_users = await asyncio.gather(
         async_fetch_users(),
         async_fetch_older_users()
     )
+
+    print("\n📦 All Users:")
+    for row in users:
+        print(row)
+
+    print("\n🎯 Users Older Than 40:")
+    for row in older_users:
+        print(row)
 
 if __name__ == "__main__":
     asyncio.run(fetch_concurrently())
