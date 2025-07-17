e631738fe21cbd7af5861520b9a6d4cd71791eba
Emmanuel5012 <osomisakin@gmail.com>
2025-07-17 20:40:18 +0100

    Updating the db

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
@@ -5,28 +6,39 @@ from django.utils import timezone
 
 # Create your models here.
 
+#Custom user model to extend the default Django user model
 class CustomUser(AbstractUser):
-    full_name = models.CharField(max_length=100)
-    is_online = models.BooleanField(default=False)
+    user_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    username = None  # Disable username field
+    USERNAME_FIELD = 'email'  # Use email as the unique identifier
+    REQUIRED_FIELDS = ['first_name', 'last_name']  # Required fields for user creation
 
     def __str__(self):
-        return self.username
+        return f"{self.first_name} {self.last_name})"
     
 # Conversation model to represent a chat between users
 class Conversation(models.Model):
+    conversation_id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
     participants = models.ManyToManyField(CustomUser, related_name='conversations')
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Conversation between {self.pk}"
+        return f"Conversation between {self.conversation_id}"
     
 # Message model to represent messages in a conversation
 class Message(models.Model):
-    conversation = models.ForeignKey(Conversation, related_name='messages', on_delete=models.CASCADE)
-    sender = models.ForeignKey(CustomUser, related_name='sent_messages', on_delete=models.CASCADE)
-    content = models.TextField()
-    timestamp = models.DateTimeField(default=timezone.now)
+    message_id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='sent_messages')
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(default=timezone.now)
     is_read = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"Message from {self.sender.username} in conversation {self.conversation.pk}"
+        return f"Message from {self.sender.email}"
