c0ff305af76ca5d92050979267f672206ca1f18e
Emmanuel5012 <osomisakin@gmail.com>
2025-07-17 21:09:47 +0100

    adding serializers

---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import CustomUser, Conversation, Message
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = CustomUserSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Conversation
+        fields = '__all__'
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = CustomUserSerializer(read_only=True)
+    conversation = ConversationSerializer(read_only=True)
+
+    class Meta:
+        model = Message
+        fields = '__all__'
+        read_only_fields = ('sender', 'conversation', 'sent_at', 'is_read')
