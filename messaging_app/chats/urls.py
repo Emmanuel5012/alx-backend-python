1c34ea543588b4dd5afd90e1c816298e3bd39f57
Emmanuel5012 <osomisakin@gmail.com>
2025-07-17 22:15:33 +0100

    Adding nested routes

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter, NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
 # Explicit: routers.DefaultRouter()
 router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
+
+# Nested router for messages within conversations
+conversation_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversation_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api/', include(conversation_router.urls)),
 ]
--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -20,4 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),  # Include the URLs from the chats app
+
+    # Add browsable API login/logout
+    path('api-auth/', include('rest_framework.urls')),
 ]
