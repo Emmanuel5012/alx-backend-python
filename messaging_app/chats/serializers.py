8a55d0a35673008879f49aefc73835104c56e879
Emmanuel5012 <osomisakin@gmail.com>
2025-07-17 21:28:00 +0100

    adding serializers

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -2,23 +2,29 @@ from rest_framework import serializers
 from .models import CustomUser, Conversation, Message
 
 class CustomUserSerializer(serializers.ModelSerializer):
+    full_name = serializers.CharField(source='get_full_name', read_only=True)
     class Meta:
         model = CustomUser
-        fields = '__all__'
+        fields = ['user_id', 'email','first_name', 'last_name', 'full_name', 'phone_number']
 
+class MessageSerializer(serializers.ModelSerializer):
+    sender_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Message
+        fields = ['message_id', 'sender', 'sender_name', 'message_body', 'sent_at', 'is_read']
+        
+    def get_sender_name(self, obj):
+        return f"{obj.sender.get_full_name()} ({obj.sender.email})"
 class ConversationSerializer(serializers.ModelSerializer):
     participants = CustomUserSerializer(many=True, read_only=True)
     messages = MessageSerializer(many=True, read_only=True)
 
     class Meta:
         model = Conversation
-        fields = '__all__'
+        fields = ['conversation_id', 'participants', 'messages', 'created_at']
 
-class MessageSerializer(serializers.ModelSerializer):
-    sender = CustomUserSerializer(read_only=True)
-    conversation = ConversationSerializer(read_only=True)
-
-    class Meta:
-        model = Message
-        fields = '__all__'
-        read_only_fields = ('sender', 'conversation', 'sent_at', 'is_read')
+    def validate(self, data):
+        if 'participants' not in data or len(data['participants']) < 2:
+            raise serializers.ValidationError("A conversation must have at least two participants.")
+        return data
