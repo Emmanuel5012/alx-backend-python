d34f0990fbf92a14e35315c8338e1112a0616694
Emmanuel5012 <osomisakin@gmail.com>
2025-07-20 11:34:51 +0100

    Adding a new line

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -29,6 +29,7 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
         self.assertEqual(str(context.exception), f"'{path[-1]}'")
+        
 
 class TestGetJson(unittest.TestCase):
     """Test get_json function"""
