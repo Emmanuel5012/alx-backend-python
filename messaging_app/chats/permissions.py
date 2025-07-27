238f9d97af67b9616307dad689e289fe5ffc4814
Emmanuel5012 <osomisakin@gmail.com>
2025-07-27 14:15:56 +0100

    Add Permissions

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions
+from .models import Conversation
 
 
 class IsOwner(permissions.BasePermission):
@@ -14,3 +15,22 @@ class IsOwner(permissions.BasePermission):
             return request.user in obj.participants.all()
         return False
     
+
+class IsParticipantOfConversation(permissions.BasePermission):
+    """
+    Custom permission to check if the user is a participant of the conversation.
+    """
+
+    def has_permission(self, request, view):
+        # Check if the user is authenticated
+        return request.user and request.user.is_authenticated
+    
+    def has_object_permission(self, request, view, obj):
+        # Handle for Message objects linked to a Conversation
+        if hasattr(obj, 'conversation'):
+            return request.user in obj.conversation.participants.all()
+        
+        # Handle for Conversation objects
+        if hasattr(obj, 'participants'):
+            return request.user in obj.participants.all()
+        return False
