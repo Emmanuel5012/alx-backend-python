431e3b821f2db187491ae94b61460b7a4b595c0d
Emmanuel5012 <osomisakin@gmail.com>
2025-07-27 15:42:12 +0100

    Pagination and Filtering

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,4 +1,5 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 class MessagePagination(PageNumberPagination):
     """
@@ -8,3 +9,13 @@ class MessagePagination(PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 100
     page_query_param = 'page'
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,
+            'current_page': self.page.number,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
